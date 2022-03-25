df39685bd5f626cc35e74f6fa38b264485e84e5e
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-25 19:32:35 +0100

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,20 @@
+# Explains & gives examples on different loops to use in a program.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+# An example of a for loop 
+# A for loop is used to display something a set amount of times.
+# x is a variable that is used as a counter 
+
+for x in range(10): # This repeats the message "Good morning" 10 times.
+    print("Good morning") # This is the message
+    
+# An example of a while loop
+# A while loop repeats until a condition is met
+
+Num = 0 # Num is a variable
+
+while(Num < 10): # This repeats the message "Good morning" until num is less than 10.
+    print("Good morning") # Message
+    Num = Num + 1 # Increase the value of the variable Num
