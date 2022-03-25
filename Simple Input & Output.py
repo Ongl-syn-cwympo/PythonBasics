4c32aaddb858ffae11fe9b396facb5ffff17df10
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-25 19:12:19 +0100

    Create Simple Input & Output.py

---
+++ Simple Input & Output.py
@@ -0,0 +1,30 @@
+# Asks the user to input their name & age and the program will save it and output it.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+# Program asks for user name 
+# Program prints the message "Good morning" + the entered name
+# This printed message will show up on screen
+
+# This asks for the user to enter their name (string)
+Name = input("What's your name?") # The inputed message is saved in the variable "Name"
+print("Good morning", Name) # The saved variable can be outputed, as shown here.
+
+# We can make this a bit more complicated by adding age into the mix and outputing both of them.
+
+# Program asks for user age 
+# Program prints the entered age and "nice number!"
+# This printed message will show up on screen
+
+# This asks for the user to enter their age (integer)
+Age = input("What's your age?") # The inputed message is saved in the variable "Age"
+print(Age, "nice number!") # The saved variable can be outputed, as shown here.
+
+# We can then output both of these variables together.
+
+# Displayes both variables ~ The values assigned by the user
+print(Name, Age) 
+
+# What's a variable?
+# A variable is a container for different types of data (like integer, float, String and etc...).
