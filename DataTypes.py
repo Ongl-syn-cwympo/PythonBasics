fa4d3d0b0e811f4217357867c330168be3d28e2c
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-26 23:11:58 +0100

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,24 @@
+# Data types ~ floating point, integer, string, boolean
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+# Floating point ~ This is a decimal number.
+
+Test_Average = 65.5 
+
+# Integer ~ A whole number
+
+my_grade = 97 
+
+# String ~ A sequence of characters
+
+Output = "Hello world!"
+
+# Boolean ~ True or False, Yes or No, 1 or 0
+
+Homework_Is_Done = True
+
+# With this data, we can access it by the variables name, for example:
+
+print(Test_Average) # This will output 65.5, this value you can change, this would make the output different. 
