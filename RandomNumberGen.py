196eb9f2ba26a618cdb6bd89678ff740ab93a63f
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-27 00:18:30 +0100

    Create RandomNumberGen.py

---
+++ RandomNumberGen.py
@@ -0,0 +1,10 @@
+# Random number generator ~ Generates and displays a random number, from a range chosen by the programmer.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+import random 
+
+random_number = random.randint(1, 100) # These numbers represent the minimum and maximum number that is allowed to be displayed.
+print(random_number) # This is the variable that displays the random number.
+#The randint() method returns an integer number selected element from the specified range.
