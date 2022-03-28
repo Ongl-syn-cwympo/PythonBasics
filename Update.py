3ec363e0f6682561f0a005901f130b7a0522a7ab
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-28 12:25:13 +0200

    Create Update.py

---
+++ Update.py
@@ -0,0 +1,9 @@
+# Update values
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+import config
+
+config.a = 10          # updating value of a to 10
+config.b = "alphabet"  # updating value of b to "alphabet"
