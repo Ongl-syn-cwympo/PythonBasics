bcb4990b10fc8e6e8dbb86de2ee1a3650c4e98b3
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-28 10:12:25 +0200

    Create TypeConversions.py

---
+++ TypeConversions.py
@@ -0,0 +1,15 @@
+# Type conversions
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+x = input("x: ")
+
+# You have to explicitly tell python what you want your objects to be converted into. 
+
+print(int(x))
+print(float(x))
+print(bool(x))
+  
+
+  
