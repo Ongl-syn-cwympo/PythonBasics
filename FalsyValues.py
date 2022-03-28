48e9f2b3fc16b8b452b5c4909f80ed508f1183c0
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-28 10:07:08 +0200

    Create FalsyValues.py

---
+++ FalsyValues.py
@@ -0,0 +1,14 @@
+# Falsy values - empty string '' empty list [] 0 None - apply not to a false, get true.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+#falsy values
+
+y = []
+z = ''
+x = 0
+y = None
+
+
+print(bool(y), bool(z), bool(x), bool(y))
